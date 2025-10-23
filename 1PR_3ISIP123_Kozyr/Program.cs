ee28b690fcd0bf7d4f8400440c5a8676515c6dd9
Антон Козырь <antail.for.game@gmail.com>
2025-10-23 11:03:25 +0300

    Добавил класс предметов

--- 1PR_3ISIP123_Kozyr/Program.cs
+++ 1PR_3ISIP123_Kozyr/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Game game = new Game();
+game.Start();
+public class Item
+{
+    public string Name { get; set; }
+    public int Attack { get; set; }
+    public int Defense { get; set; }
+
+    public Item(string name, int attack = 0, int defense = 0)
+    {
+        Name = name;
+        Attack = attack;
+        Defense = defense;
+    }
+}
