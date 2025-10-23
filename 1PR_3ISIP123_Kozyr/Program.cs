ef83dcf5ed5db7f69d8917ef556da6ab05213769
Антон Козырь <antail.for.game@gmail.com>
2025-10-23 11:18:07 +0300

    Добавил класс игрока

--- 1PR_3ISIP123_Kozyr/Program.cs
+++ 1PR_3ISIP123_Kozyr/Program.cs
@@ -44,6 +44,22 @@ public class Enemy
         FreezeChance = freezeChance;
         IgnoreDefense = ignoreDefense;
     }
-
     public bool IsAlive() => HP > 0;
+}
+public class Player
+{
+    public int MaxHP { get; set; } = 100;
+    public int HP { get; set; } = 100;
+    public Weapon Weapon { get; set; }
+    public Armor Armor { get; set; }
+    public bool IsFrozen { get; set; } = false;
+    public Player()
+    {
+        Weapon = new Weapon("Кулаки", 5);
+        Armor = new Armor("Простая одежда", 2);
+    }
+    public int GetAttack() => Weapon.Attack;
+    public int GetDefense() => Armor.Defense;
+    public bool IsAlive() => HP > 0;
+    public void Heal() => HP = MaxHP;
 }
