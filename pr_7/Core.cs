5e7fb980f869b30f5a83e7355fadc05f1f159d59
Антон Козырь <antail.for.game@gmail.com>
2025-10-30 11:15:17 +0300

    Создал классы сущностей

---
+++ pr_7/Core.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr_7
+{
+    internal class Core
+    {
+        public static Pr_7_GordEntities Context = new Pr_7_GordEntities();
+    }
+
+}
--- pr_7/Program.cs
+++ pr_7/Program.cs
@@ -1,15 +1,43 @@
-﻿using System;
+﻿using pr_7;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace pr_7
+namespace AutoService
 {
-    internal class Program
+    public class Part
     {
-        static void Main(string[] args)
-        {
-        }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class Warehouse
+    {
+        public int ID { get; set; }
+        public decimal Balance { get; set; }
+    }
+
+    public class WarehousePart
+    {
+        public int WarehouseID { get; set; }
+        public int PartID { get; set; }
+        public int Count { get; set; }
+    }
+
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string CarModel { get; set; }
+        public int BrokenPartID { get; set; }
+        public decimal RepairCost { get; set; }
+        public bool IsServed { get; set; }
+    }
+
+    public class PurchaseOrder
+    {
+        public int PartID { get; set; }
+        public int Quantity { get; set; }
+        public int RemainingCars { get; set; }
     }
 }
