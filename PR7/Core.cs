234fa009f84218d11d890f38fde919df08b88b81
Антон Козырь <antail.for.game@gmail.com>
2025-10-30 11:12:25 +0300

    Создал классы сущностей

---
+++ PR7/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR7
+{
+    internal class Core
+    {
+        public static Pr_7_GordEntities Context = new Pr_7_GordEntities();
+    }
+}
