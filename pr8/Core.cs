d6e5c28fe940e2cdaabd248d9cad6e540dfc287e
Антон Козырь <antail.for.game@gmail.com>
2025-11-06 11:39:36 +0300

    Создал главное меню

---
+++ pr8/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr8
+{
+    internal class Core
+    {
+        public static PR_8_GordovEntities Context = new PR_8_GordovEntities();
+    }
+}
---
+++ pr8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Маркетплейс GMWOG");
+
+            while (true)
+            {
+                Console.WriteLine("\nГлавное меню:");
+                Console.WriteLine("1 - Регистрация");
+                Console.WriteLine("2 - Вход");
+                Console.WriteLine("3 - Товары");
+                Console.WriteLine("4 - Выход");
+                Console.Write("Ваш выбор: ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1": Register(); break;
+                    case "2": Login(); break;
+                    case "3": ShowProducts(); break;
+                    case "4": return;
+                    default: Console.WriteLine("Неверный выбор!"); break;
+                }
+            }
+        }
+    }
+}
