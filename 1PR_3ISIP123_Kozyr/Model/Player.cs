f855db89eb34a77f85c759d0434774925e0e8d8c
Антон Козырь <antail.for.game@gmail.com>
2025-11-20 11:40:25 +0300

    сделал класс игрока

---
+++ 1PR_3ISIP123_Kozyr/Model/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1PR_3ISIP123_Kozyr.Model
+{
+    public class Player
+    {
+        public int MaxHP { get; set; } = 100;
+        public int HP { get; set; } = 100;
+        public Weapon Weapon { get; set; }
+        public Armor Armor { get; set; }
+        public bool IsFrozen { get; set; } = false;
+
+        public Player(WeaponFactory weaponFactory, ArmorFactory armorFactory)
+        {
+            Weapon = weaponFactory.CreateStarterWeapon();
+            Armor = armorFactory.CreateStarterArmor();
+        }
+
+        public int GetAttack() => Weapon.Attack;
+        public int GetDefense() => Armor.Defense;
+        public bool IsAlive() => HP > 0;
+        public void Heal() => HP = MaxHP;
+    }
+}
